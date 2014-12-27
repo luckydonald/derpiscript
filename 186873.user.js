06dadd949de6a5195d15d75f9f7ea0cb0b6ce9b8
luckydonald <luckydonald@flutterb.at>
2014-12-27 04:24:49 +0100

    0.1.3.9 (beta): notification, if new update is available.

--- 186873.user.js
+++ 186873.user.js
@@ -24,10 +24,12 @@
 // @grant          GM_getValue
 // @grant          GM_setValue
 // @grant          GM_listValues
+// @grant          GM_xmlhttpRequest
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @updateURL      https://resources.flutterb.at/userscript/186873.user.js
 // @downloadURL    https://resources.flutterb.at/userscript/186873.user.js
-// @version        0.1.3.8
+// @version        0.1.3.9
+// @history        0.1.3.9 notification, if new update is available.
 // @history        0.1.3.8 setting now attached to normal derpibooru settings tabs. Click "Derpiscript" tab to access the settings. | added different tag coloring for watched, spoilered and hidden tags. | added rudimentary error display when script Version detections randomly dies. Error seems to be a Greasemonkey related bug. (Error: Greasemonkey access violation: unsafeWindow cannot call GM_*).
 // @history        0.1.3.7 fixed likeButton, now working again. | settings moved to derpibooru settings page. | changelog will now be displayed on version change. | now parsing the @history tags to have changelog in one place. | clicking on version string in settings now opens changelog window too.
 // @history        0.1.3.6 added settings to keep Button on top or on bottom. One step further being iPad/iOS/Android/MobilePhone ready! | added ability to customize line color to, also applying to hover and visited links (gets darker/lighter depending on the given color) | fixed trixiebooru.org domain.
@@ -125,6 +127,100 @@ doVersionCheck(lastScriptVersion, scriptVersion);
 var page = getPageType(window.location.pathname);
 console.log(page);
 doPageType(page);
+checkAutoUpdate();
+
+
+
+
+
+function checkAutoUpdate(){
+	infostring = GM_info.scriptMetaStr;
+	infostring = infostring.replace(/\r?\n/g, "\n");
+	infostring = infostring.replace(/\r/g, "\n");
+	lines = infostring.split("\n"); //linebreak
+	var updateURL = "";
+	for(var i = 0; i< lines.length; i++){
+		var line = lines[i];
+		line = line.trim();
+		if(line.indexOf("//") > -1){
+			line = line.substr(line.indexOf("//") + 2);
+			line = line.trim();
+			if(line.indexOf("@updateURL") > -1){
+				line = line.substr(line.indexOf("@updateURL") + 10);
+				line = line.trim();
+				updateURL = line;
+				console.log("old@updateURL:" + line);
+			}
+		}
+	}
+	updateURL = "https://resources.flutterb.at/userscript/186873.1.3.7.user.js";
+	GM_xmlhttpRequest({
+	method:"GET",
+	url:updateURL,
+	onload:function(details) {
+		if (details.status == 200) {
+			console.log("old@version:" + GM_info.script.version);
+			var metaBlock = new Array();
+			var infostring = details.responseText;
+			var newestVersion = "";
+			var newestUpdateURL = "";
+			var metaBlockStarted = false;
+			infostring = infostring.replace(/\r?\n/g, "\n");
+			infostring = infostring.replace(/\r/g, "\n");
+			lines = infostring.split("\n"); //linebreak
+			for(var i = 0; i< lines.length; i++){
+				var line = lines[i];
+				line = line.trim();
+				if(line.indexOf("//") > -1){
+					line = line.substr(line.indexOf("//") + 2);
+					line = line.trim();
+					if(line.indexOf("==UserScript==") > -1){
+						metaBlockStarted = true;
+						continue;
+					}
+					if(line.indexOf("==/UserScript==") > -1){
+						break;
+					}
+					if(!metaBlockStarted) {
+						continue;
+					}
+					if(line.indexOf("@updateURL") > -1){
+						line = line.substr(line.indexOf("@updateURL") + 10);
+						line = line.trim();
+						newestUpdateURL = line;
+						console.log("new@updateURL:" + line);
+					}
+					if(line.indexOf("@version") > -1){
+						line = line.substr(line.indexOf("@version") + 8);
+						line = line.trim();
+						newestVersion = line;
+						console.log("new@version:" + line);
+					}
+					metaBlock[metaBlock.length] = lines[i];
+				}
+			}
+			applyUpdate(metaBlock, newestVersion, newestUpdateURL);
+		}
+	}
+	});
+	
+}
+
+function applyUpdate(metaBlock, newestVersion, newestUpdateURL){
+	console.log({meta: metaBlock, newest: newestVersion, url:newestUpdateURL});
+	var diff = versionCompare(scriptVersion, newestVersion, null);
+	if(diff != NaN && diff>0){ //FIXME
+		showChangelogVersion(scriptVersion, newestVersion, metaBlock.join("\n"), newestUpdateURL, true);
+	}
+}
+
+
+
+
+
+
+
+
 
 //alert("Daring Do is bestpony"); //or maybe Maud, now in season 4...
 
@@ -150,7 +246,7 @@ function doVersionCheck(lastVersion, newVersion){
 		return null;
 	}
 	if(lastVersion != newVersion) {
-		var item = showChangelogVersion(lastVersion, newVersion);
+		var item = showChangelogVersion(lastVersion, newVersion, GM_info.scriptMetaStr, "", false);
 		GM_setValue('lastScriptVersion', newVersion);
 		return item;
 	}
@@ -159,10 +255,10 @@ function hideChangelog(){
 	changelog_bg.style.display = "none";
 }
 function showChangelog(){
-	showChangelogVersion(lastScriptVersion, scriptVersion);
+	showChangelogVersion(lastScriptVersion, scriptVersion, GM_info.scriptMetaStr, "", false);
 }
 
-function showChangelogVersion(lastVersion, newVersion){
+function showChangelogVersion(lastVersion, newVersion, infostring, updateURL, isUpdate) {
 	var css = "\
 		#changelog-bg {																  \n\
 			color:black;                                                              \n\
@@ -224,7 +320,6 @@ function showChangelogVersion(lastVersion, newVersion){
 		}								\n\
 		";
 	applyStyle(css, "changelog");
-	infostring = GM_info.scriptMetaStr;
 	infostring = infostring.replace(/\r?\n/g, "\n");
 	infostring = infostring.replace(/\r/g, "\n");
 	lines = infostring.split("\n"); //linebreak
@@ -254,11 +349,23 @@ function showChangelogVersion(lastVersion, newVersion){
 	}
 	var html = "\
 		<div id = \"changelog-box\"> \n\
-		<div class=\"title\">Derpiscript Changelog</div> \n\																																																																										\n\
-		Current Version: <b>" + newVersion + "</b><br />\n\
-		Last    Version: <b>" + lastVersion + "</b>\n\
-		<ul> \n\
 		";
+	if (isUpdate){
+		html+="\
+			<div class=\"title\">New Version available.</div> \																																																																										\n\
+			Current Version: <b>" + lastVersion + "</b>\
+			New     Version: <b>" + newVersion + "</b><br />\
+			<a href=\"" + updateURL + "\">Update</a>\
+			<ul> \
+		";
+	}else{
+		html+="\
+			<div class=\"title\">Derpiscript Changelog</div> \																																																																										\n\
+			Current Version: <b>" + newVersion + "</b><br />\
+			Last    Version: <b>" + lastVersion + "</b>\
+			<ul> \
+		";
+	}
 	var wasNewVersion = false;
 	var wasLastVersion = false;
 	for(var i = 0; i< changelog.length; i++){
@@ -269,11 +376,16 @@ function showChangelogVersion(lastVersion, newVersion){
 		html += "<li class=\"" + (wasLastVersion ? " old":"") + "\"> \n";
 		html += "<div class=\"versionstring" + (newVersion == version ? " new": "") + (lastVersion == version ? " last": "") + (wasLastVersion ? " old":"") + "\">Version "+ version + ":</div> \n";
 		wasLastVersion = (lastVersion == version ? true : wasLastVersion);
+		if (isUpdate && wasLastVersion){
+			html += "</li>";
+			break;
+		}
 		html += "<ul class=\"" + (wasLastVersion ? " old":"") + "\"> \n";
 		for(var j = 0; j < textarray.length; j++){
 			html += "<li>" + textarray[j] + "</li> \n";
 		}
-		html += "</ul> \n";		html += "</li>";
+		html += "</ul> \n";
+		html += "</li>";
 	}
 	html += "</ul></div>";
 	//html += "<input type=\"button\" value=\"close\"></input>";
@@ -393,6 +505,85 @@ function removeDownloaded(pictureNumberAsString){
 // Libraries:
 // Libraries:
 
+
+//TODO: support "beta" 
+//  http://stackoverflow.com/a/6832721
+/**
+ * Compares two software version numbers (e.g. "1.7.1" or "1.2b").
+ *
+ * This function was born in http://stackoverflow.com/a/6832721.
+ *
+ * @param {string} v1 The first version to be compared.
+ * @param {string} v2 The second version to be compared.
+ * @param {object} [options] Optional flags that affect comparison behavior:
+ * <ul>
+ *     <li>
+ *         <tt>lexicographical: true</tt> compares each part of the version strings lexicographically instead of
+ *         naturally; this allows suffixes such as "b" or "dev" but will cause "1.10" to be considered smaller than
+ *         "1.2".
+ *     </li>
+ *     <li>
+ *         <tt>zeroExtend: true</tt> changes the result if one version string has less parts than the other. In
+ *         this case the shorter string will be padded with "zero" parts instead of being considered smaller.
+ *     </li>
+ * </ul>
+ * @returns {number|NaN}
+ * <ul>
+ *    <li>0 if the versions are equal</li>
+ *    <li>a negative integer iff v1 < v2</li>
+ *    <li>a positive integer iff v1 > v2</li>
+ *    <li>NaN if either version string is in the wrong format</li>
+ * </ul>
+ *
+ * @copyright by Jon Papaioannou (["john", "papaioannou"].join(".") + "@gmail.com")
+ * @license This function is in the public domain. Do what you want with it, no strings attached.
+ */
+function versionCompare(v1, v2, options) {
+    var lexicographical = options && options.lexicographical,
+        zeroExtend = options && options.zeroExtend,
+        v1parts = v1.split('.'),
+        v2parts = v2.split('.');
+
+    function isValidPart(x) {
+        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
+    }
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        return NaN;
+    }
+
+    if (zeroExtend) {
+        while (v1parts.length < v2parts.length) v1parts.push("0");
+        while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length == i) {
+            return 1;
+        }
+
+        if (v1parts[i] == v2parts[i]) {
+            continue;
+        }
+        else if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
+    return 0;
+}
 // http://james.padolsey.com/javascript/debug-jquery-events-with-listhandlers/
 /*
 // List all onclick handlers of all anchor elements:
